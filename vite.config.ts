78e4a4ab88d63cb5afb005b024ece29785eefe95
THIAGO LIMA CAVALCANTE <44018@pmenos.com.br>
2026-03-25 19:55:37 -0300

    fix: ajuste PWA icons e manifest

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,12 @@ export default defineConfig({
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       }
